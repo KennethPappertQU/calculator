e57fd76f7051ff94a016d0c0edf8c32725540ede
KennethPappertQU <Kenneth.Pappert@quinnipiac.edu>
2026-02-11 19:38:46 -0500

    Subtraction

--- Calculator.java
+++ Calculator.java
@@ -9,12 +9,31 @@ public class Calculator {
         System.out.print("Enter another number: ");
         int secondNum = in.nextInt();
 
-        System.out.println(addNums(firstNum, secondNum));
+        System.out.print("Would you like to add or subtract? ");
+        String response = in.next();
+
+        switch (response) {
+            case "add":
+            case "addition":
+                System.out.println(addNums(firstNum, secondNum));
+                break;
+            case "subtract":
+            case "subtraction":
+                System.out.println(subtractNum(firstNum, secondNum));
+                break;
+            default:
+                System.out.println("Incorrect Input");
+                break;
+        }
+
         in.close();
     }
 
     public static int addNums (int first, int second) {
         return first + second;
+    }
+    public static int subtractNum (int first, int second) {
+        return first - second;
     }
 }
 
