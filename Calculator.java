73ceba270906c1b153d081a5ce53bf5c9eda5895
KennethPappertQU <Kenneth.Pappert@quinnipiac.edu>
2026-02-11 19:23:47 -0500

    Addition

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String [] args) {
-        System.out.println("Hello World!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int firstNum = in.nextInt();
+
+        System.out.print("Enter another number: ");
+        int secondNum = in.nextInt();
+
+        System.out.println(addNums(firstNum, secondNum));
+        in.close();
+    }
+
+    public static int addNums (int first, int second) {
+        return first + second;
     }
 }
 
